71ff8fead90a616d920ad908c0c09bf887f0d95d
Carlos Gomes <dev.shinsei@gmail.com>
2023-01-25 17:37:16 -0300

    feat: routes, models, middlewares, controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
-import userModel from "../models/user.model"
+import userModel from "../models/user.model.js"
 import jsonwebtoken from "jsonwebtoken"
-import responseHandler from "../handlers/response.handler"
+import responseHandler from "../handlers/response.handler.js"
 
 const signup = async (req, res) => {
   try {
@@ -36,7 +36,61 @@ const signin = async (req, res) => {
     const user = await userModel.findOne({ username }).select("username password salt id displayName")
 
     if (!user) return responseHandler.badrequest(res, "Username or password is incorrect.")
+
+    if (!user.validPassword(password)) return responseHandler.badrequest(res, "Username or password is incorrect.")
+
+    const token = jsonwebtoken.sign({ data: user.id }, process.env.TOKEN_SECRET, { expiresIn: "24h" });
+
+    user.password = undefined;
+    user.salt = undefined;
+
+    responseHandler.created(res, {
+      token,
+      ...user._doc,
+      id: user.id
+    });
+
   } catch {
     responseHandler.error(res)
   }
+}
+
+const updatePassword = async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    const user = await userModel.findById(req.user.id).select("password id salt")
+
+    if (!user) return responseHandler.unauthorize(res)
+
+    if (!user.validPassword(password)) return responseHandler.badrequest(res, "Wrong Password")
+
+    user.setPassword(newPassword);
+
+    await user.save();
+
+    responseHandler.ok(res)
+
+  } catch {
+    responseHandler.error(res)
+  }
+}
+
+const getInfo = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user.id).select("username displayName id")
+
+    if (!user) return responseHandler.unauthorize(res)
+
+    responseHandler.ok(res, user)
+  } catch {
+    responseHandler.error(res)
+  }
+}
+
+export default {
+  signup,
+  signin,
+  updatePassword,
+  getInfo
 }
